54058db7947d7cf7eba4bb44888dea130efbef78
SaraToth <saratoth101@gmail.com>
2025-07-01 19:39:57 +0900

    Feat: Set up app

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const passport = require("passport");
+require("./config/passport"); // Passport strategy setup
+
+const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// To parse data from POST requests
 app.use(express.urlencoded({ extended: false}));
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+// Initialize Passport
+app.use(passport.initialize());
 
+// Routes
+app.use("/", indexRouter);
+
+// Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send(err);
