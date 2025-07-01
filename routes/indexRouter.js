7fb237de03990d9d7a20f6b0f8094990b057fb6a
SaraToth <saratoth101@gmail.com>
2025-07-02 07:01:44 +0900

    Feat: Post clubhouse form route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const indexRouter = Router();
-const { getIndex, getSignup, getLogin, postLogin, postSignup, getLogout } = require("../controllers/indexController");
+const { getIndex, getSignup, getLogin, postLogin, postSignup, getLogout, postClubhouse } = require("../controllers/indexController");
 
 indexRouter.get("/login", getLogin);
 indexRouter.post("/login", postLogin);
 indexRouter.get("/sign-up", getSignup);
 indexRouter.post("/sign-up", postSignup);
 indexRouter.get("/logout", getLogout);
+indexRouter.post("/", postClubhouse);
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
