babd570bab978a33e73c1bb1279d3241c6c8406a
SaraToth <saratoth101@gmail.com>
2025-07-02 08:58:55 +0900

    Feat: Form valiation for newMessage form

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,9 +1,21 @@
 const asyncHandler = require("express-async-handler");
-const { body, validationResult, matchedData } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const queries = require("../db/queries");
 const passport = require("passport");
 require("dotenv").config();
+
+const validateNewMessage = [
+    body("title")
+        .trim()
+        .notEmpty().withMessage("Title cannot be empty")
+        .isLength({max: 60 }).withMessage("Title must be no more than 120 characters"),
+
+    body("content")
+        .trim()
+        .notEmpty().withMessage("Message content cannot be empty")
+        .isLength({max: 300 }).withMessage("Message cannot exceed 300 characters"),
+];
 
 const getMessages = (req, res) => {
     res.render("messages", {user: req.user});
@@ -13,9 +25,17 @@ const getNewMessage = (req, res) => {
     res.render("newMessage", {user: req.user});
 }
 
-const postNewMessage = asyncHandler(async (req, res) => {
-    await queries.postNewMessage(req.body.title, req.body.content, req.user.id);
-    res.redirect("/messages");
-});
+const postNewMessage = [
+    validateNewMessage,
+
+    asyncHandler(async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("newMessage", {user: req.user, errors: errors.array()});
+        }
+        await queries.postNewMessage(req.body.title, req.body.content, req.user.id);
+        res.redirect("/messages");
+    }),
+];
 
 module.exports = { getMessages, getNewMessage, postNewMessage };
