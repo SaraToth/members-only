621ed80c57c7e8451a2e5a792b5ceedf6b5136ea
SaraToth <saratoth101@gmail.com>
2025-07-02 07:26:41 +0900

    Fix: routes and exports that no longer exist

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,40 +6,6 @@ const passport = require("passport");
 require("dotenv").config();
 
 
-// Sanitizer for names to be stored as proper nouns
-const toProperNoun = (rawName) => {
-    return rawName
-        .toLowerCase()
-        .replace(/\b\w/g, char => char.toUpperCase());
-};
-
-const validateSignUp = [
-    body("firstname")
-        .trim()
-        .isAlpha().withMessage("First name must only contain letters")
-        .customSanitizer(toProperNoun),
-
-    body("lastname")
-        .trim()
-        .isAlpha().withMessage("Last name must only contain letters")
-        .customSanitizer(toProperNoun),
-
-    body("username")
-        .normalizeEmail()
-        .isEmail().withMessage("Username must be a valid email address"),
-
-    body("password")
-        .trim()
-        .isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
-        .matches(/[a-z]/).withMessage("Password must contain at least one lower case letter")
-        .matches(/[A-Z]/).withMessage("Password must contain at least one upper case letter")
-        .matches(/[0-9]/).withMessage("Password must contain at least one number")
-        .matches(/[^A-Za-z0-9]/).withMessage("Password must contain at least one special character"),
-    
-    body("confirmPassword").trim()
-        .custom((value, {req}) => value === req.body.password).withMessage("Passwords do not match"),
-];
-
 const validateClubhouseCode = [
     body("clubhouseCode")
     .trim()
@@ -51,29 +17,12 @@ const getIndex = (req, res) => {
     res.render("index", { user: req.user});
 };
 
-const getSignup = (req, res) => {
-    res.render("sign-up");
-};
 
 const getLogin = (req, res) => {
     const origin = req.query.origin;
     res.render("login", {origin});
 };
 
-const postSignup = [
-    validateSignUp,
-
-    asyncHandler(async (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).render("sign-up", { errors: errors.array()});
-        }
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        await queries.createUser(req.body.firstname, req.body.lastname, req.body.username, hashedPassword);
-        res.redirect("/login?origin=signup");
-    }),
-];
 
 const postLogin = (req, res, next) => {
     passport.authenticate("local", {
@@ -114,12 +63,5 @@ const postClubhouse = [
     }),
 ];
 
-const getMessages = (req, res) => {
-    res.render("/messages");
-}
 
-const getNewMessage = (req, res) => {
-    res.render("/messages/new");
-}
-
-module.exports = { getIndex, getSignup, getLogin, postSignup, postLogin, getLogout, postClubhouse, getMessages, getNewMessage };
+module.exports = { getIndex, getLogin, postLogin, getLogout, postClubhouse };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,15 +1,15 @@
 const { Router } = require("express");
 const indexRouter = Router();
-const { getIndex, getSignup, getLogin, postLogin, postSignup, getLogout, postClubhouse, getMessages, getNewMessage } = require("../controllers/indexController");
+const { getIndex, getLogin, postLogin, getLogout, postClubhouse } = require("../controllers/indexController");
 
-indexRouter.get("/messages", getMessages);
-indexRouter.get("/messages/new", getNewMessage);
+// Login & Logout routes
 indexRouter.get("/login", getLogin);
 indexRouter.post("/login", postLogin);
-indexRouter.get("/sign-up", getSignup);
-indexRouter.post("/sign-up", postSignup);
 indexRouter.get("/logout", getLogout);
+
+// Members who enter the clubhouse code gain a membership upgrade
 indexRouter.post("/", postClubhouse);
+
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
