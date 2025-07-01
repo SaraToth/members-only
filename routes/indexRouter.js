c62329bcb68954a80dc23f76cd01e0bf99d07332
SaraToth <saratoth101@gmail.com>
2025-07-01 21:48:13 +0900

    Feat: Create a route to logout

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const indexRouter = Router();
-const { getIndex, getSignup, getLogin, postLogin, postSignup } = require("../controllers/indexController");
+const { getIndex, getSignup, getLogin, postLogin, postSignup, getLogout } = require("../controllers/indexController");
 
 indexRouter.get("/login", getLogin);
 indexRouter.post("/login", postLogin);
 indexRouter.get("/sign-up", getSignup);
 indexRouter.post("/sign-up", postSignup);
+indexRouter.get("/logout", getLogout);
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
