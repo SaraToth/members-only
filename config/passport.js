697d89f02c37f17ba6c684301d5c515d9ab42c2d
SaraToth <saratoth101@gmail.com>
2025-07-01 21:19:33 +0900

    Feat: Config passport with db/queries

--- config/passport.js
+++ config/passport.js
@@ -2,13 +2,12 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const pool = require("../db/pool");
 const bcrypt = require("bcryptjs");
+const queries = require("../db/queries");
 
 passport.use(
     new LocalStrategy(async (username, password, done) => {
         try {
-            const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-            const user = rows[0];
-
+            await queries.findUserByUsername(username);
             if (!user) {
                 return done(null, false, { message: "Incorrect username"});
             }
